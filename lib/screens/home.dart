627dc231752f04a8759387100f7f589a9ae19472
hackerhgl <hackerhgl@gmail.com>
2021-08-21 17:26:01 +0500

    Columns

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -5,6 +5,31 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text("Home screen");
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Home screen"),
+      ),
+      body: Container(
+        color: Colors.blue,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Container(
+              color: Colors.red,
+              padding: EdgeInsets.symmetric(
+                vertical: 10,
+                horizontal: 50,
+              ),
+              child: Text("Symmetric"),
+            ),
+            Text("Hello 1"),
+            Text("Hello 2"),
+            Text("Hello 3"),
+            Text("wow"),
+          ],
+        ),
+      ),
+    );
   }
 }
