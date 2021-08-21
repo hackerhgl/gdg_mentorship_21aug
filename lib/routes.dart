cacdd9a6102e5e3ecb2d1c516dedcf40c8fceb49
hackerhgl <hackerhgl@gmail.com>
2021-08-21 17:13:47 +0500

    import home screen

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
 
+import 'screens/home.dart';
+
 class AppRoutes extends StatelessWidget {
   const AppRoutes({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Text("Hello"),
+      initialRoute: "home",
+      routes: {
+        "home": (_) => HomeScreen(),
+      },
     );
   }
 }
